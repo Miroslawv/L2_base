75afc1522e0e6f9123d0eb9f7d1da358140ae443
Miroslawv <vajtehovicmiroslav@gmail.com>
2025-09-26 09:06:04 +0300

    Выполнил четырнадцатое задание

---
+++ task_14.js
@@ -0,0 +1,12 @@
+function isCircleSorted( arr ){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++)
+    {
+        let next = (i + 1) % arr.length;
+        if (arr[i] > arr[next])
+        {
+            count++;
+        }
+    }
+    return count <= 1;
+}
